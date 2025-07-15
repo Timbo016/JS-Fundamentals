a9a0c0157d5fd14d1fef4e02dacf84539e1047f2
Babatunde Opoola <Opoolatimileyin@gmail.com>
2025-07-15 15:29:17 +0100

    Create 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+
+const languages = [
+    "C is fun",
+    "Python is cool",
+    "JavaScript is amazing"
+];
+
+// Loop through the array and print each string
+for (let i = 0; i < languages.length; i++) {
+    console.log(languages[i]);
+}
