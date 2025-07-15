e03b51169df369027899e32b83da22d60b1dfff9
Babatunde Opoola <Opoolatimileyin@gmail.com>
2025-07-15 13:48:44 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,12 +1,10 @@
 const args = process.argv.slice(2);
 const numberOfArguments = args.length;
 
-if(numberOfArguments === 0){
-    console.log("No Argument");
-}
-else if(numberOfArguments === 1){
-    console.log("Argument Found");
-}
-else {
-    console.log("Arguments Found");
+if (numberOfArguments === 0) {
+    console.log("No argument");
+} else if (numberOfArguments === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
 }
