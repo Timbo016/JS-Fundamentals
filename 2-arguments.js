7a9f90b05bb68745caabe1f4fb575bd0e8a27e25
Babatunde Opoola <Opoolatimileyin@gmail.com>
2025-07-15 13:09:01 +0100

    Create 2-arguments.js
    Added the third file

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+let score = 20;
+
+ if(score >= 10 && score <= 15){
+    console.log("No Argument");
+}
+else if(score >= 16 && score <= 20){
+    console.log("Argument Found");
+}
+else{
+    console.log("Error Occured");
+}
